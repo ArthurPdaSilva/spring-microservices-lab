35741e43e913f0b262992923f29ee6cab3466e9b
ArthurPdaSilva <arthurnpdasilva@gmail.com>
2026-09-01 17:38:35 -0300

    api gateway

---
+++ services/api-gateway/src/test/java/br/com/estudos/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
