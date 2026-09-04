5b556dc7f12c9ef55643508396aa197aae0b1d3e
ArthurPdaSilva <arthurnpdasilva@gmail.com>
2026-09-04 14:48:29 -0300

    feat: add CORS configuration and update OpenAPI settings for book and exchange services

--- services/api-gateway/src/main/java/br/com/estudos/apigateway/config/OpenApiConfiguration.java
+++ services/api-gateway/src/main/java/br/com/estudos/apigateway/config/OpenApiConfiguration.java
@@ -1,30 +1,41 @@
-package br.com.estudos.apigateway.config;
+//Caso eu queira descobrir as rotas do gateway, sem usar o application.yml, posso fazer via código, mas para isso preciso de um bean do tipo RouteLocator
 
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Configuration
-public class OpenApiConfiguration {
-
-    @Bean
-    public SwaggerUiConfigParameters swaggerUiConfigParameters(SwaggerUiConfigProperties swaggerUiConfigProperties) {
-        return new SwaggerUiConfigParameters(swaggerUiConfigProperties);
-    }
-
-    @Bean
-    @Lazy(false)
-    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters swaggerUiConfigParameters,
-                                     RouteDefinitionLocator routeDefinitionLocator) {
-        var routeDefinitions = routeDefinitionLocator.getRouteDefinitions().collectList().block();
-        var groups = new ArrayList<GroupedOpenApi>();
-        return groups;
-    }
-}
+//package br.com.estudos.apigateway.config;
+//
+//import org.springdoc.core.models.GroupedOpenApi;
+//import org.springdoc.core.properties.SwaggerUiConfigParameters;
+//import org.springdoc.core.properties.SwaggerUiConfigProperties;
+//import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Lazy;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Configuration
+//public class OpenApiConfiguration {
+//
+//    @Bean
+//    public SwaggerUiConfigParameters swaggerUiConfigParameters(SwaggerUiConfigProperties swaggerUiConfigProperties) {
+//        return new SwaggerUiConfigParameters(swaggerUiConfigProperties);
+//    }
+//
+//    @Bean
+//    @Lazy(false)
+//    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters swaggerUiConfigParameters,
+//                                     RouteDefinitionLocator routeDefinitionLocator) {
+//        var routeDefinitions = routeDefinitionLocator.getRouteDefinitions().collectList().block();
+//        var groups = new ArrayList<GroupedOpenApi>();
+//
+//        if(routeDefinitions != null) {
+//          routeDefinitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+//              String name = routeDefinition.getId().replaceAll("-service", "");
+//              swaggerUiConfigParameters.addGroup(name);
+//              groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+//          });
+//        }
+//
+//        return groups;
+//    }
+//}
--- services/exchange-service-microsservice/src/main/java/br/com/estudos/exchangeservicemicrosservice/config/OpenApiConfiguration.java
+++ services/exchange-service-microsservice/src/main/java/br/com/estudos/exchangeservicemicrosservice/config/OpenApiConfiguration.java
@@ -5,18 +5,25 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition(info = @Info(
         title = "Exchange Service API",
         version = "1.0",
         description = "API for managing currency exchange rates and conversions"
 ))
+@Configuration
 public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().components(
                 new Components()
+        ).servers(
+                List.of(new Server().url("/exchange-service-microsservice"))
         ).info(
                 new io.swagger.v3.oas.models.info.Info()
                         .title("Exchange Service API")
