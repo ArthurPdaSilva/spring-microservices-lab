10fea44f0591bb4a7baebc7198a5bef6758ef569
ArthurPdaSilva <arthurnpdasilva@gmail.com>
2026-09-05 19:09:26 -0300

    feat: add Zipkin tracing configuration and update docker-compose for Zipkin service

--- services/exchange-service-microsservice/src/main/java/br/com/estudos/exchangeservicemicrosservice/services/ExchangeService.java
+++ services/exchange-service-microsservice/src/main/java/br/com/estudos/exchangeservicemicrosservice/services/ExchangeService.java
@@ -3,6 +3,8 @@ package br.com.estudos.exchangeservicemicrosservice.services;
 import br.com.estudos.exchangeservicemicrosservice.dtos.ExchangeResponseDTO;
 import br.com.estudos.exchangeservicemicrosservice.enviromnent.InstanceInformationService;
 import br.com.estudos.exchangeservicemicrosservice.repositories.ExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.math.BigDecimal;
 
 @Service
 public class ExchangeService {
+    private Logger log = LoggerFactory.getLogger(ExchangeService.class);
+
     @Autowired
     private ExchangeRepository exchangeRepository;
 
@@ -17,6 +21,8 @@ public class ExchangeService {
     private InstanceInformationService instanceInformationService;
 
     public ExchangeResponseDTO getExchange(BigDecimal amount, String from, String to) {
+        log.info("getExchange called with amount: {}, from: {}, to: {}", amount, from, to);
+
         if (amount == null || from == null || to == null) {
             throw new RuntimeException("Currency Unsupported");
         }
